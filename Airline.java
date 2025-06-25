5c511e435b3063c55a4dac5fb8fd90c5d9860ee5
Nusaibha Hasan <166198352+Nusaibha@users.noreply.github.com>
2025-06-24 21:33:41 -0400

    Create Airline.java

---
+++ Airline.java
@@ -0,0 +1,14 @@
+// Each airline has a name
+public class Airline {
+    private String name;
+
+    // Constructor to initialize the airline name
+    public Airline(String name) {
+        this.name = name;
+    }
+
+    // Getter for the airline name
+    public String getName() {
+        return name;
+    }
+}
